7884895e2d67ed11da855b4e4986f317c55c9b8e
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2026-07-16 21:33:37 -0300

    Creating first api

--- Django/students/views.py
+++ Django/students/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def students(request):
+    students = [
+        {"id":1, "name":"John doe", "age": 35}
+    ]
+    return HttpResponse(students)
