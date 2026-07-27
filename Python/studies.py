b00e4dbed419d0a44cc485b7912b1411538ccc80
Antony dos Santos Felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2026-07-27 19:52:07 -0300

    Python basics

--- Python/studies.py
+++ Python/studies.py
@@ -1,10 +1,11 @@
+#-----------------------------------------------------------------PRINTS-----------------------------------------------------#
 print("Hello World") #Hello
 
 print(12, 34)
 print(12, 34, sep="-")
 print(12, 34, sep="-", end="\r\n")
 print(12, 34, sep="-", end="\n")
-
+#-----------------------------------------------------------------VARIABLES-----------------------------------------------------#
 var = 2 + 2
 adicao = 10+10
 modulo = 55 % 2
@@ -15,33 +16,38 @@ multiplicacao = 10 * 10
 divisao_inteira = 10//2.2
 integer = int("1")
 string = "hello"
+complexo = 3j
 a_dez_vezes = "A" * 10
 concatenacao = "A" + "B" + "C"
 trez_vezes_letter = 3 * "Letter"
-
-print(len(string))
-print(f"String invertida {string[::-1]}")
+endereco_memoria_v1 = "a"
+endereco_memoria_v2 = "a"
+endereco_memoria_v3 = "a"
+#-----------------------------------------------------------------PRINT, FUNCTIONS AND VARS-----------------------------------------------------#
 print(var)
 print(adicao)
 print(subtracao)
-print(multiplicacao)
 print(divisao)
-print(divisao_inteira)
-print(exponenciacao)
 print(modulo)
-print(concatenacao)
+print(len(string))
 print(a_dez_vezes)
+print(concatenacao)
+print(multiplicacao)
+print(exponenciacao)
+print(divisao_inteira)
 print(trez_vezes_letter)
+print(f"String invertida {string[::-1]}")
 print(12, 34, adicao, "hello", sep="-", end="\n")
 
 print(int("1"),type(int("1")))
 print(type(float("1")+1))
 print(bool(" "))
 print(str(11) + "b")
-
+print(id(endereco_memoria_v1), id(endereco_memoria_v2), id(endereco_memoria_v3))
+#-----------------------------------------------------------------INPUTS-----------------------------------------------------#
 valor_um = int(input("Digite um valor: "))
 valor_dois = int(input("Digite outro valor: "))
-
+#-----------------------------------------------------------------CONDITIONALS-----------------------------------------------------#
 if " " in input("Digite algo: "):
     print("O que digitou contem espaços")
 
@@ -59,8 +65,15 @@ elif valor_dois > valor_um:
     )
 else:
     print("Valores iguais")
+#-----------------------------------------------------------------LISTS, TUPLES, SETS AND DICTS-----------------------------------------------------#
+lists = [1, 2, 3, 3]
+print("Lists:", lists)
 
-endereco_memoria_v1 = "a"
-endereco_memoria_v2 = "a"
-endereco_memoria_v3 = "a"
-print(id(endereco_memoria_v1), id(endereco_memoria_v2), id(endereco_memoria_v3))
+tuples = (1, 2, 3, 4, 5)
+print("Tuples:", tuples)
+
+sets = {1, 2, 3, 3}
+print("Sets:", sets)
+
+dicts = {"chave": "valor"}
+print("Dicts:", dicts)
