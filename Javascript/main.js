4820b6a708faa5b66eaadcc4f0364d1f0bda671f
Antony dos Santos Felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2026-07-27 21:59:43 -0300

    Javascript studies

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,3 +1,5 @@
+//<script src="main.js" defer type="module"/> //defer makes the script load just after the html and type module so that the js is treated as a module 
+
 /*------------------------------------------VARIABLES AND OBJECTS------------------------------------------*/
 var name = 'Gwyn';
 var age = 29;
@@ -16,6 +18,9 @@ const person = {
 
 baseUser = "Artorias"
 baseAge = 30;
+/*------------------------------------------DOM FUNCTION------------------------------------------*/
+document.getElementById("demo").innerHTML = "Hello";
+alert("boy")
 /*------------------------------------------FUNCTIONS------------------------------------------*/
 function summarizeUser(userName, userAge, userHasHobby) {
   return (
@@ -25,3 +30,20 @@ function summarizeUser(userName, userAge, userHasHobby) {
 
 console.log(summarizeUser(name, age, hasHobbies));
 person.greet();
+
+console.log(Date())
+/*------------------------------------------JSON FUNCTIONS------------------------------------------*/
+const text = '{"name":"Artorias", "age":30, "city":"New York"}';
+const personData = JSON.parse(text);
+
+const employee = {
+  name: "Artorias",
+  age: 30,
+  city: "New York"
+};
+const textEmployee = JSON.stringify(person);
+/*------------------------------------------XML FUNCTIONS------------------------------------------*/
+const text = "<person><name>Gwyn</name></person>";
+const parser = new DOMParser();
+const xmlDoc = parser.parseFromString(text, "text/xml");
+const name = xmlDoc.getElementsByTagName("name")[0].textContent;
